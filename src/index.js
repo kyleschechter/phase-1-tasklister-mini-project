0383b720e18aa058fdfa5fddbf1c0a546ecc4be8
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-05-28 10:51:48 -0400

    Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const newTask = document.getElementById('create-task-form')
+  const listOfTasks = document.getElementById('tasks')
+
+  newTask.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newInput = e.target.querySelector('#new-task-description').value
+    const taskList = document.createElement('li')
+    taskList.innerText = newInput
+    listOfTasks.append(taskList)
+  })
+})
